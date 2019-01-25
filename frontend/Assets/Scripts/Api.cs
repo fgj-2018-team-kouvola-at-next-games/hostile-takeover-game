85c70a7a6c3221b3ebde4213701a53ad89e9b92d
Jesse Luoto <jesse.luoto@gmail.com>
2019-01-25 22:37:05 +0200

    Add socket.io to client, simple connection to server

---
+++ frontend/Assets/Scripts/Api.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO;
+
+
+public class Api {
+	private static SocketIOComponent _socket;
+
+	public static SocketIOComponent instance {
+		get {
+			if (Api._socket == null) {
+				GameObject go = GameObject.Find("SocketIO");
+				Api._socket = go.GetComponent<SocketIOComponent>();
+			}
+
+			return Api._socket;
+		}
+	}
+
+	public static void SendMoveMessage(Vector2 delta) {
+		JSONObject message = new JSONObject (JSONObject.Type.OBJECT);
+		message.AddField ("x", delta.x);
+		message.AddField ("y", delta.y);
+		message.AddField ("player", "p1");
+
+		Api.instance.Emit ("move", message);
+	}
+}
---
+++ frontend/Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			Api.SendMoveMessage (Vector2.up);
+		}
+		if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			Api.SendMoveMessage (Vector2.down);
+		}
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			Api.SendMoveMessage (Vector2.left);
+		}
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			Api.SendMoveMessage (Vector2.right);
+		}
+	}
+}
