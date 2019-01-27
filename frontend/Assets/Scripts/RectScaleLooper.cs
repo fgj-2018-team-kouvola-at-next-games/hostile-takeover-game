bb6e2b1c4b46bb2cc7daec16b16cded442ea454b
Jarkka Salminen <jarkka.salminen@gmail.com>
2019-01-27 12:44:35 +0200

    Added: Small ping pong effect to Menu Slap SPACE to  Play text

---
+++ frontend/Assets/Scripts/RectScaleLooper.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RectScaleLooper : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //gameObject.GetComponent<Text>().fontSize = Mathf.RoundToInt(Mathf.Repeat(Time.time, 10)) + 26;
+        float newSize = Mathf.PingPong(Time.time * 0.1f, 0.08f) + 1;
+        gameObject.GetComponent<RectTransform>().localScale = new Vector3(newSize, newSize, 1);
+    }
+}
