d2d7fa500b513d154a485578df2406b48108d4e6
Jarkka Salminen <jarkka.salminen@gmail.com>
2019-01-27 10:16:29 +0200

    Added: Simple main menu and game starting logics via Space bar

---
+++ frontend/Assets/Scripts/AppManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+using UnityEngine;
+
+public class AppManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    public void StartGame() {
+        SceneManager.LoadScene("Game");
+    }
+}
---
+++ frontend/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour {
+
+    private AppManager appManager;
+
+    void Start() {
+        appManager = GameObject.FindObjectOfType<AppManager>();    
+    }
+
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            appManager.StartGame();
+        }
+    }
+}
